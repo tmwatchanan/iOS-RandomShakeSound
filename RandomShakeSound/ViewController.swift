22b6dfe18354297c976e6e158c793648a207e67f
Tommie <Tommie@tommies-mac.local>
2017-02-15 17:32:20 +0700

    Added the whole files

--- RandomShakeSound/ViewController.swift
+++ RandomShakeSound/ViewController.swift
@@ -7,8 +7,13 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player = AVAudioPlayer()
+    
+    let sounds = ["boing", "explosion", "hit", "knife", "shoot", "swish", "wah", "warble"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +25,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        if event?.subtype == UIEventSubtype.motionShake {
+            let randomNumber = Int(arc4random_uniform(UInt32(sounds.count)))
+            let audioPath = Bundle.main.path(forResource: sounds[randomNumber], ofType: "mp3")
+            do {
+                try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+            }
+            catch {
+                // error
+            }
+            player.play()
+        }
+    }
 }
-
